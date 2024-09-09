53d96b3ed9fb1410e5f230bf47b70ca792c3e225
Koundinya <123861193+koundinya2002@users.noreply.github.com>
2024-09-09 14:14:40 +0530

    version 2.3

--- theblog/models.py
+++ theblog/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
-#from datetime import datetime, date
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
